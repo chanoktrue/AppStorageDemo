0ca2d41114c8a5235e67545af39f9c36c16753a5
Thongchai Subsaidee <chanoktrue@me.com>
2022-02-26 17:51:30 +0700

    first project

---
+++ AppStroageDemo/AppStroageDemo/AppStroageDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStroageDemoApp.swift
+//  AppStroageDemo
+//
+//  Created by Thongchai Subsaidee on 26/2/22.
+//
+
+import SwiftUI
+
+@main
+struct AppStroageDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStroageDemo/AppStroageDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppStroageDemo
+//
+//  Created by Thongchai Subsaidee on 26/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
