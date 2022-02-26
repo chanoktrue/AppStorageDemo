c4e5536180baf2f0e355a41de2100074732c35f5
Thongchai Subsaidee <chanoktrue@me.com>
2022-02-26 18:02:23 +0700

    finish demo

--- AppStroageDemo/AppStroageDemo/ContentView.swift
+++ AppStroageDemo/AppStroageDemo/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @AppStorage("name") var name = "Content view"
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                NavigationLink(
+                    destination: View1(), label: {
+                        Text("Go to view1: \(name)")
+                    })
+                
+                Button {
+                    name = "add from Content view"
+                } label: {
+                    Text("Save")
+                }
+                .padding()
+            }
             .padding()
+            .navigationTitle("Home")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+struct View1: View {
+    @AppStorage("name") var name = ""
+    var body: some View {
+        NavigationLink (destination: View2()) {
+            Text("Go to view2 name : \(name)")
+        }
+        
+        Button {
+            name = "add from view1"
+        } label: {
+            Text("Save")
+        }
+        .padding()
+        
+    }
+}
+
+
+struct View2: View {
+    @AppStorage("name") var name = ""
+    var body: some View {
+        Text("View2 name: \(name)")
+        
+        Button {
+            name = "add from view2"
+        } label: {
+            Text("Save")
+        }
+        .padding()
+
     }
 }
